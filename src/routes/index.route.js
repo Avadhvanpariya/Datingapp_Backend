86d4259b69e855753cde4304c6f4b3e69fec56e4
Avadhvanpariya <avadhvanpariya123@gmail.com>
2026-05-27 11:09:44 +0530

    save local changes

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,13 +1,7 @@
 const express = require('express');
 const authRouter = require('./auth/authIndex.route');
-const userRouter = require('./user/userIndex.route');
-const profilesRouter = require('./profiles.route');
-const matchesRouter = require('./matches.route');
-const followerRouter = require('./followers.route');
-const postsRouter = require('./posts.route');
-const chatsRouter = require('./chats.route');
-const webrtcRouter = require('./webrtc.route');
-const coinsRouter = require('./coins.route');
+const userDepartment = require('./user/userIndex.route');
+const socialDepartment = require('./social/socialIndex.route');
 
 const router = express.Router();
 
